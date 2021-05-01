8ff5a91b8c533eed197cf2799d93366e091548e2
Manuel Panichelli <panicmanu@gmail.com>
2021-05-01 19:28:26 -0300

    Modificaciones al script

---
+++ 1-wiretapping/README.md
@@ -0,0 +1,5 @@
+# Taller 1 - Wire Tapping
+
+Links:
+
+- Numeros de protocolos: https://www.iana.org/assignments/ieee-802-numbers/ieee-802-numbers.xhtml
--- 1-wiretapping/taller1.py
+++ 1-wiretapping/taller1.py
@@ -1,24 +1,52 @@
 #!/usr/bin/env python3
 
-from scapy.all import *
+"""
+extender el código para que calcule la información de cada símbolo y la entropía
+de la fuente.
+"""
+
+import math
+import scapy.all as scapy
+
 S1 = {}
 
 def mostrar_fuente(S):
     N = sum(S.values())
     simbolos = sorted(S.items(), key=lambda x: -x[1])
-    print("\n".join([ " %s : %.5f" % (d,k/N) for d,k in simbolos ]))
+    print("\n".join([ f"{d}: p: {k/N:.5f}, i: {-math.log(k/N):.5f}" for d,k in simbolos ]))
+    
+    entropia = 0
+    for _, k in simbolos:
+        entropia += -math.log(k/N) * k/N
+
+    print("Entropia:", entropia)
+    print(N)
     print()
 
-def callback(pkt):
-    if pkt.haslayer(Ether):
-        dire = "BROADCAST" if pkt[Ether].dst=="ff:ff:ff:ff:ff:ff" else "UNICAST"
-        proto = pkt[Ether].type # El campo type del frame tiene el protocolo
-        s_i = (dire, proto) # Aca se define el simbolo de la fuente
+    if N > 15000:
+        quit()
+
+def analyze_pkg(pkt):
+    if pkt.haslayer(scapy.Ether):
+        dire = "BROADCAST" if pkt[scapy.Ether].dst=="ff:ff:ff:ff:ff:ff" else "UNICAST"
+        proto = pkt[scapy.Ether].type       # El campo type del frame tiene el protocolo
+        s_i = (dire, proto)                 # Aca se define el simbolo de la fuente
 
     if s_i not in S1:
         S1[s_i] = 0.0
-        S1[s_i] += 1.0
 
+    S1[s_i] += 1.0
+
+def callback(pkt):
+    analyze_pkg(pkt)
     mostrar_fuente(S1)
 
-sniff(prn=callback)
+def main():
+    # Para filtrar
+    # sniff(prn=arp_monitor_callback, filter="arp", store=0)
+    scapy.sniff(prn=callback)
+
+
+if __name__ == "__main__":
+    main()
+
