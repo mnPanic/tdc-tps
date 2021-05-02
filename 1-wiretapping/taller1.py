a2e2dd83efd8816255da0a52db2689aa9779bd2a
Manuel Panichelli <panicmanu@gmail.com>
2021-05-01 21:37:06 -0300

    Extension opcional (fuente s2) del programa

--- 1-wiretapping/taller1.py
+++ 1-wiretapping/taller1.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
-"""
-extender el código para que calcule la información de cada símbolo y la entropía
-de la fuente.
-"""
+MAX_PACKETS = 15000
+MAX_PACKETS_S2 = 100
 
 import math
 import scapy.all as scapy
 
 S1 = {}
+S2_src = {}
+S2_dst = {}
 
 def mostrar_fuente(S):
     N = sum(S.values())
@@ -23,9 +23,6 @@ def mostrar_fuente(S):
     print(N)
     print()
 
-    if N > 15000:
-        quit()
-
 def analyze_pkg(pkt):
     if pkt.haslayer(scapy.Ether):
         dire = "BROADCAST" if pkt[scapy.Ether].dst=="ff:ff:ff:ff:ff:ff" else "UNICAST"
@@ -37,15 +34,42 @@ def analyze_pkg(pkt):
 
     S1[s_i] += 1.0
 
+def analyze_pkg_S2(pkt):
+    ip_src = pkt[scapy.ARP].psrc
+    ip_dst = pkt[scapy.ARP].pdst
+
+    if ip_dst not in S2_dst:
+        S2_dst[ip_dst] = 0
+    
+    S2_dst[ip_dst] += 1
+
+    if ip_src not in S2_src:
+        S2_src[ip_src] = 0
+    
+    S2_src[ip_src] += 1
+
+def callback_s2(pkt):
+    analyze_pkg_S2(pkt)
+
+    print("src:")
+    mostrar_fuente(S2_src)
+    print("dst:")
+    mostrar_fuente(S2_dst)
+
+    if sum(S2_src.values()) == MAX_PACKETS_S2:
+        quit()
+
 def callback(pkt):
     analyze_pkg(pkt)
     mostrar_fuente(S1)
 
-def main():
-    # Para filtrar
-    # sniff(prn=arp_monitor_callback, filter="arp", store=0)
-    scapy.sniff(prn=callback)
+    if sum(S1.values()) == MAX_PACKETS:
+        quit()
 
+def main():
+    #scapy.sniff(prn=callback)
+
+    scapy.sniff(prn=callback_s2, filter="arp", store=0)
 
 if __name__ == "__main__":
     main()
