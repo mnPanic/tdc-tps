6f969dd242cc22f7e27d0d4ed4a2af2ad645c754
Elias Cerdeira <eliascerdeira@gmail.com>
2021-05-15 21:09:21 -0300

    Add rtt by hops

--- 2-traceroute/taller2.py
+++ 2-traceroute/taller2.py
@@ -6,16 +6,15 @@ import scapy.all as scp
 import time
 
 
-TTL_MAX = 31
+TTL_MAX = 30
 ITERATIONS = 5
 
-hops = [{} for i in range(TTL_MAX)]
-output = [{} for i in range(TTL_MAX)]
+hops = [{} for i in range(TTL_MAX + 1)]
+output = [{} for i in range(TTL_MAX + 1)]
 
-print("HOLA, ESTE ES EL TP DE TRACEROUTE")
 print()
 
-for ttl in range(1, TTL_MAX):
+for ttl in range(1, TTL_MAX + 1):
     for i in range(ITERATIONS):
         request = scp.IP(dst=sys.argv[1], ttl=ttl) / scp.ICMP()
 
@@ -45,3 +44,33 @@ for ttl in range(1, TTL_MAX):
         print(f'{ttl:2} {output[ttl]["IP"]:15} \t\t {output[ttl]["median"]:.2f} ms \t {output[ttl]["sd"]:.2f} ms')
     else:
         print(f'{ttl:2} * * *')
+
+# RTT by hops
+
+print()
+print("RTT por saltos")
+
+rtt_hops = []
+
+for ttl in range(1, TTL_MAX):
+    if "median" not in output[ttl]:
+        rtt_hops.append(-1)
+        continue
+
+    actual_rtt = output[ttl]["median"]
+    next_rtt = 0
+
+    for next_ttl in range(ttl, TTL_MAX):
+        if "median" not in output[next_ttl]:
+            continue
+
+        next_rtt = output[next_ttl]["median"]
+
+        if next_rtt - actual_rtt > 0:
+            break
+
+
+    rtt_hops.append(next_rtt - actual_rtt)
+
+for step, rtt_hop in enumerate(rtt_hops):
+    print(f'{step+1:2} {rtt_hop:.2f}')
